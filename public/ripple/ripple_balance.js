e32596a6fa9be39f251fb292b37f8ef13af19321
Maedeh Zebhi <maedehzebhi@gmail.com>
2023-11-12 11:58:34 +0330

    debugging ripple

--- public/ripple/ripple_balance.js
+++ public/ripple/ripple_balance.js
@@ -1,5 +1,5 @@
 const RippleAPI = require('ripple-lib').RippleAPI;
-var adminaddress = process.argv[2];
+var address = process.argv[2];
 
 const Ripple = new RippleAPI({
 	server: 'wss://s2.ripple.com', // Public rippled server
@@ -17,12 +17,30 @@ Ripple.on('disconnected', function (code) {
 	console.log('disconnected, code:', code);
 });
 
-Ripple.connect().then(function () {
-	return Ripple.getServerInfo();
-}).then(function (server_info) {
+// Ripple.connect().then(function () {
+// 	return Ripple.getServerInfo();
+// }).then(function (server_info) {
 
-	Ripple.getBalances(adminaddress).then(function (transaction) {
-		console.log(JSON.stringify(transaction));
-		//process.exit(-1);
-	}).catch(console.error);
-}).catch(console.error);
+// 	Ripple.getBalances(adminaddress).then(function (transaction) {
+// 		console.log(JSON.stringify(transaction, null, 2));
+// 		//process.exit(-1);
+// 	}).catch(console.error);
+// }).catch(console.error);
+
+
+async function getBalances() {
+	try {
+		await Ripple.connect();
+
+		//? Get the balances for the specified address
+		const balances = await api.getBalances(address);
+		console.log(balances);
+		const balancesJSON = JSON.stringify(balances, null, 2);
+		await Ripple.disconnect();
+		console.log(balancesJSON);
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+getBalances();
