ef8665119544d6706110a35e109ada09ccb45250
Maedeh Zebhi <maedehzebhi@gmail.com>
2023-11-12 09:58:34 +0330

    remove exit process

--- public/ripple/ripple.js
+++ public/ripple/ripple.js
@@ -7,23 +7,24 @@ const Ripple = new RippleAPI({ server: 'wss://s2.ripple.com' });
 
 
 Ripple.on('error', function (errorCode, errorMessage) {
-console.log(errorCode + ': ' + errorMessage);
-resp.json('{"status":0,"msg":"Unable to withdraw, problem occured. '+errorMessage+'."}');
+	console.log(errorCode + ': ' + errorMessage);
+	//resp.json('{"status":0,"msg":"Unable to withdraw, problem occured. '+errorMessage+'."}');
 });
 
 Ripple.on('connected', function () {
-console.log('connected');
+	console.log('connected');
 });
 Ripple.on('disconnected', function (code) {
-// code - close code sent by the server
-// will be 1000 if this was normal closure
-console.log('disconnected, code:', code);
+	// code - close code sent by the server
+	// will be 1000 if this was normal closure
+	console.log('disconnected, code:', code);
 });
 Ripple.connect().then(function () {
-console.log('ripple connected');
-return Ripple.getServerInfo();
-// insert code here //
+	console.log('ripple connected');
+	return Ripple.getServerInfo();
+	// insert code here //
 }).then(function (server_info) {
 	var rippleAddress = Ripple.generateAddress();
-	console.log(rippleAddress);process.exit(-1);
+	console.log(rippleAddress);
+	//process.exit(-1);
 }).catch(console.error);
