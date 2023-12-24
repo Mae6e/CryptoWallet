c07c986ec2881833dd71a888b80ce8e3fd80ad85
mae6e <maedehzebhi@gmail.com>
2023-12-24 11:01:50 +0000

    fixed updateLastStatusOfNetwork function

--- repositories/networkRepository.js
+++ repositories/networkRepository.js
@@ -6,11 +6,11 @@ exports.getNetworkByType = async (type) => {
     return await Network.findOne({ type }, { type: 1, siteWallet: 1 });
 }
 
-exports.updateLastStatusOfNetwork = async (id, lastBlockNumber, lastExecutedAt) => {
+exports.updateLastStatusOfNetwork = async (id, lastBlockNumber) => {
     try {
         return await Network.findOneAndUpdate({ _id: id }, {
             $set: {
-                lastExecutedAt,
+                lastExecutedAt: new Date(),
                 lastBlockNumber
             }
         }, { new: true });
